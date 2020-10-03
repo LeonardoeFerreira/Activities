80038eb54a8facbcfed02064b377c64209f53b1d
Leonardo Ferreira <leonardo.ferreira01@fatec.sp.gov.br>
2020-10-03 17:03:51 -0300

    Update Como passar dados entre activity.java

--- Como passar dados entre activity.java
+++ Como passar dados entre activity.java
@@ -17,3 +17,49 @@ Bundle extras = getIntent().getExtras();
 if (extras != null) {
     String value = extras.getString("key");
 }
+
+<<=========================================================================================================================>>>
+
+/*
+*Como retornar dados da Activity chamada para aquela que a chamou?
+* Utilizar startActivityForResult() em vez de startActivity().
+*/
+
+//Na primeira Activity:
+static final int ACTIVITY_2_REQUEST = 1;
+
+Intent i = new Intent(this, SegundaActivity.class);
+startActivityForResult(i, ACTIVITY_2_REQUEST);
+
+
+//Na segunda Activity:
+//Quando tiver o resultado pronto para ser devolvido à primeira activity
+
+String resultado = seuTextView.getText().toString();
+Intent returnIntent = new Intent();
+returnIntent.putExtra("resultado",resultado);
+setResult(RESULT_OK,returnIntent);
+
+
+//Para receber o valor faça o override do método onActivityResult() na primeira Activity
+
+@override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    if (requestCode == ACTIVITY_2_REQUEST) {
+        if(resultCode == RESULT_OK){
+            String resultado = data.getStringExtra("resultado");
+            //Coloque no EditText
+            seuEditText.setText(resultado);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
