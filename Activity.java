538665b3eb939989edf4259275bcb35b3a936021
Leonardo Ferreira <leonardo.ferreira01@fatec.sp.gov.br>
2020-10-03 16:54:04 -0300

    Add files via upload

---
+++ Activity.java
@@ -0,0 +1,12 @@
+1ª Activity:
+
+Intent i = new Intent(Activity.this, NewActivity.class);
+i.putExtra("key", value);
+startActivity(i);
+
+2ª Activity:
+
+Bundle extras = getIntent().getExtras();
+if (extras != null) {
+    String value = extras.getString("key");
+}
